5255aca9ea90fa249b4d188a33c8169e1a7e38e3
vpmouler <vpmouler@gmail.com>
2017-09-15 16:30:31 -0700

    BO4465E$

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -14,6 +14,12 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  var output;
+  fs.readFileSync(__dirname + '/public/index.html', 'utf8', function(err, data) {
+    output = data;
+  });
+  console.log(output);
+  return output;
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,25 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var helpers = require('./http-helpers');
+var fs = require('fs');
+// var indexhtml = fs.createReadStream(__dirname + '/public/index.html');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  fs.createReadStream(__dirname + '/public/index.html').pipe(res);
+  // var data = '';
+  
+  // indexhtml.on('data', function(chunk) {
+  //   data += chunk;
+  // });
+
+  // indexhtml.on('end', function() {
+  //   res.end(data);
+  // });
+  // res.end(
+  //   fs.readFileSync(__dirname + '/public/index.html', 'utf8', function(err, data) {
+  //     return data;
+  //   })
+  // );
 };
